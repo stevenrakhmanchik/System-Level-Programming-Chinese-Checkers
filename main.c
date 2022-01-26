fd05b50f1e56f388944f2831b6a586546691eb51
Steven Rakhmanchik <48323187+stevenrakhmanchik@users.noreply.github.com>
2022-01-26 08:07:56 -0500

    added commands for linux sound

--- main.c
+++ main.c
@@ -23,12 +23,12 @@ void print_image(FILE *fptr);
 #define MAX_LEN 128;
 #define DEF_BOARD_SIDE 5;
 
-	ifdef __linux__
-	char gameover[] = " ";
-	char menu[] = " ";
-	char cursormove[] = " ";
-	char next[] = " ";
-	endif
+#ifdef __linux__
+	char gameover[] = "aplay sounds/gameover.mp3";
+	char menu[] = "aplay sounds/menu.wav";
+	char cursormove[] = "aplay sounds/move.wav";
+	char next[] = "aplay sounds/next.wav";
+#endif
 
 #ifdef __APPLE__
 	char gameover[] = "afplay sounds/gameover.mp3";
